f125732e96e0555c3540b5c10358fea67cf3c91f
Rehema Wachira <Rehema@Rehemas-MacBook-Pro.local>
2016-08-19 17:46:17 +0300

    Notes app with amended comments

--- notes_app.py
+++ notes_app.py
@@ -7,8 +7,7 @@ class NotesApplication(object):
 
 
 	def create(self, note_content):  
-
-	#creates note instances. Tests for input types
+	#creates note instances. Tests for input types.
 
 		if not note_content:
 			print "No content provided"
@@ -26,7 +25,6 @@ class NotesApplication(object):
 
 
 	def list(self): 
-
 	#prints all the contents of the list. 
 
 		for key, value in enumerate(self.notes):
@@ -38,16 +36,18 @@ class NotesApplication(object):
 
 
 	def get(self, note_id): 
-
-	#returns the note contents at a specified index
+	'''
+	returns the note contents at a specified index
+	index starts at 0. noted_id -1 is to make sure 
+	user's request matches actual index.
+	'''
 
 		note = self.notes[note_id - 1]
 		return note
 
 
 	def search(self, search_text): 
-
-	#returns the results of a search
+	#returns the results of a search.
 
 		print 'Showing results for search < %s >' %(search_text)
 
@@ -61,16 +61,14 @@ class NotesApplication(object):
 
 
 	def delete(self, note_id): 
-
-	#deletes an entry at a specified index
+	#deletes an entry at a specified index.
 
 		for entries in self.notes:
-			self.notes.pop(note_id)
+			self.notes.pop(note_id - 1)
 
 
 	def edit(self, note_id, new_content): 
-
-	#inserts new content and replaces previous content at a specified index
+	#inserts new content and replaces previous content at a specified index.
 
 		for index, entries in enumerate(self.notes):
 			note_content.insert(note_id, new_content)
