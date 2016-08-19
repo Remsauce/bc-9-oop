4b4e9dcf09aba956e069a4742e562ad0e4320524
Rehema Wachira <Rehema@Rehemas-MacBook-Pro.local>
2016-08-19 16:19:00 +0300

    Updated notes app with amended list function

--- notes_app.py
+++ notes_app.py
@@ -6,7 +6,10 @@ class NotesApplication(object):
 		self.notes = [] 
 
 
-	def create(self, note_content):  #creates note instances. Tests for input types
+	def create(self, note_content):  
+
+	#creates note instances. Tests for input types
+
 		if not note_content:
 			print "No content provided"
 
@@ -22,34 +25,53 @@ class NotesApplication(object):
 		return self.notes
 
 
-	def list(self):
+	def list(self): 
+
+	#prints all the contents of the list. 
+
+		for key, value in enumerate(self.notes):
+			print 'Note ID: %s' %(note_id)
+			print self.notes
+			print 'By Author %s' %(author)
 		
-		print 'Note ID: %s' %(note_id)
-		print self.notes
-		print 'By Author %s' %(author)
 
 
-	def get(self, note_id):
+
+	def get(self, note_id): 
+
+	#returns the note contents at a specified index
+
 		note = self.notes[note_id - 1]
+		return note
 
 
-	def search(self, search_text):
+	def search(self, search_text): 
+
+	#returns the results of a search
+
 		print 'Showing results for search < %s >' %(search_text)
 
 		for index, entries in enumerate(self.notes):
 			if entries == search_text:
 				print 'Note ID: %s \n %s ' %(index+1, entries) 
+				print self.notes
 				print 'By Author %s' %(author)
 		else:
 			print 'No results found'
 
 
-	def delete(self, note_id):
+	def delete(self, note_id): 
+
+	#deletes an entry at a specified index
+
 		for entries in self.notes:
 			self.notes.pop(note_id)
 
 
-	def edit(self, note_id, new_content):
+	def edit(self, note_id, new_content): 
+
+	#inserts new content and replaces previous content at a specified index
+
 		for index, entries in enumerate(self.notes):
 			note_content.insert(note_id, new_content)
 
